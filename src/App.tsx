0e7349af02f18982967787f7a436721f16bc5a73
pritishvaidya <pritishvaidya94@gmail.com>
2025-01-31 22:25:33 +0530

    Test

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import {useState} from "react";
+
+const App = () => {
+   const [progressBars, updateProgressBars] = useState<number[]>([]);
+
+
+
+   return (
+       <div className="container">
+          <button></button>
+       </div>
+   )
+}
+
+export default App;
