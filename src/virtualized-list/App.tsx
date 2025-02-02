2906b317c58df5f5548457466803633223450eb6
pritishvaidya <pritishvaidya94@gmail.com>
2025-02-02 16:13:42 +0530

    Add Virtualized Async list example

--- src/virtualized-list/App.tsx
+++ src/virtualized-list/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
-import VirtualizedList from "./components/VirtualizedList";
-import data from './data'
 import "./App.css"
+import VirtualizedListAsync from "./components/VirtualizedListAsync";
+
+const fetchData = async (page: number, itemsPerPage: number = 10) => {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/photos?_page=${page}&_per_page=${itemsPerPage}`)
+    return response.json();
+}
 
 const App = () => {
-    return <div><VirtualizedList data={data} containerHeight={500} itemHeight={100}/></div>
+    return <div><VirtualizedListAsync fetchData={fetchData} containerHeight={500} itemHeight={100}/></div>
 }
 
 export default App
---
+++ src/virtualized-list/components/VirtualizedListAsync.tsx
@@ -0,0 +1,79 @@
+import {Data} from "../types";
+import React, {useCallback, useEffect, useRef, useState} from "react";
+
+const OFFSET = 100
+
+const VirtualizedListAsync = ({containerHeight, itemHeight, fetchData}: {
+    containerHeight: number,
+    itemHeight: number,
+    fetchData: (page: number) => Promise<Data[]>,
+}) => {
+    const containerRef = useRef(null);
+
+    const [loading, setLoading] = useState(false);
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [items, updateItems] = useState<Data[]>([])
+    const [page, setPage] = useState(0);
+    const visibleCount = Math.ceil(containerHeight / itemHeight);
+
+    const handleScroll = useCallback(async () => {
+        if (containerRef.current) {
+            const scrollTop = containerRef.current.scrollTop;
+            const newStartIndex = Math.max(0, Math.floor(scrollTop / itemHeight));
+            if (currentIndex !== newStartIndex) {
+                setCurrentIndex(newStartIndex);
+            }
+
+            if (scrollTop + containerHeight >= items.length * itemHeight - OFFSET && !loading) {
+                await updateList()
+            }
+        }
+    }, [containerRef.current, containerHeight, itemHeight, loading, currentIndex]);
+
+    const updateList = useCallback(async () => {
+        try {
+            setLoading(true);
+            const response = await fetchData(page + 1)
+            if (response.length === 0) return;
+            console.log({response})
+            updateItems(prevItems => [...prevItems, ...response])
+            setPage(prevState => prevState + 1)
+        } catch (ex) {
+            // Handle the error
+        } finally {
+            setLoading(false);
+        }
+    }, [loading, fetchData, page])
+
+    useEffect(() => {
+        const container = containerRef.current;
+        if (container) {
+            container.addEventListener('scroll', handleScroll)
+        }
+        return () => container.removeEventListener('scroll', handleScroll)
+    }, [handleScroll])
+
+    useEffect(() => {
+        updateList()
+    }, [])
+
+
+    return (
+        <div ref={containerRef} className="list-container" style={{height: containerHeight}}>
+            <div style={{height: items.length * itemHeight, position: "relative"}}>
+                {items.slice(currentIndex, currentIndex + visibleCount).map((item, index) => {
+                    return (
+                        <div className="list-item"
+                             style={{height: itemHeight, top: (currentIndex + index) * itemHeight}}
+                             key={item.id}>
+                            <h3>{item.title}</h3>
+                            <span>{item.description}</span>
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
+    )
+}
+
+export default VirtualizedListAsync;
